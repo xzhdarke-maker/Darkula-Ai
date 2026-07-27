22ea06bea3b77e5ec20d773637059963a094765f
xzhdarke-maker <xzhdarke@gmail.com>
2026-07-27 21:56:49 +0600

    Add configuration for channels, categories, roles, and users

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  channels: {
+    mainChat: "1401958567673725073",
+    botCommands: "1530546214146412574",
+    interviewLogs: "1531284574426890421",
+  },
+
+  categories: {
+    support: "1401958509662310425",
+    staffApply: "1482161336317382657",
+    xzhGang: "1474327557955391538",
+  },
+
+  roles: {
+    ticketSupport: "1401958484320452780",
+    xzhGang: "1458783248267087872",
+    leader: "1482331201242009651",
+    authority: "1489926630007898184",
+    operatorzz: "1489931575566139563",
+    feelThePower: "1529086979134853140",
+    coOwner: "1485651267039662181",
+  },
+
+  users: {
+    owner: "1307666797318766606",
+    girlsOwner: "1426624569782964395",
+  },
+};
