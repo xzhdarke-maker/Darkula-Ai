5b49c90525aee717cbf33f8ff57ca22f36e34b24
xzhdarke-maker <xzhdarke@gmail.com>
2026-07-28 06:31:16 +0600

    Add userinfo command to display user details

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,1 +1,27 @@
+module.exports = {
+  name: "userinfo",
 
+  aliases: [
+    "my info",
+    "who am i",
+    "about me"
+  ],
+
+  async execute(message) {
+    const member = message.member;
+    const user = message.author;
+
+    const roles = member.roles.cache
+      .filter(role => role.name !== "@everyone")
+      .map(role => role.name)
+      .join(", ");
+
+    return message.reply({
+      content:
+`👤 Username: ${user.username}
+🏷️ Display Name: ${member.displayName}
+🎭 Roles: ${roles || "None"}
+📅 Joined Date: <t:${Math.floor(member.joinedTimestamp / 1000)}:F>`
+    });
+  }
+};
