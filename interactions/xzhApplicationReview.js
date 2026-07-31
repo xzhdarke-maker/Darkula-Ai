85e6e9eb455e5a577aa45bbce68afe4be456fc07
xzhdarke-maker <xzhdarke@gmail.com>
2026-07-31 14:27:29 +0600

    Remove interview from xzhInterview after application review

--- interactions/xzhApplicationReview.js
+++ interactions/xzhApplicationReview.js
@@ -224,6 +224,8 @@ https://discord.gg/zmxx5N628w`
       : `❌ ${interaction.user} rejected this application.`,
   });
 
+  xzhInterview.interviews.delete(interview.userId);
+
   // ===== PART 2 END =====
     /* ==========================
       UPDATE INTERVIEW LOG
